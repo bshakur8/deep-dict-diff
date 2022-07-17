f47f9ef9d02b48c4e4192318039d902760a532fa
Bhaa Shakur <bhaa@vastdata.com>
2022-07-17 14:41:29 +0300

    add pyi

---
+++ dict_compare.pyi
@@ -0,0 +1,8 @@
+def update(benchmark, test, avoid_inner_order=True, **kwargs): ...
+def compare(
+    benchmark: dict,
+    test: dict,
+    key: str = None,
+    avoid_inner_order: bool = False,
+    **kwargs
+): ...
